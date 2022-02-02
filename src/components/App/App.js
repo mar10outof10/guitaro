011e5c3271bf3f291ae806c45cf49375d29c4347
Martin Halas <marhaldev@gmail.com>
2022-02-02 15:21:36 -0500

    Adding folder structure to app. Added sass

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="GuitaroContainer">
+        <div className="GuitaroWindow">
+          yass
+        </div>
+        <div className="GuitaroUI">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
