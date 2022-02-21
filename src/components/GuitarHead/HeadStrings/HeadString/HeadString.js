f1c31e196ecb45d4a83c362b38c154884f1384c5
Martin Halas <marhaldev@gmail.com>
2022-02-21 14:33:18 -0500

    Added hover (mouseEnter) event to headString

--- src/components/GuitarHead/HeadStrings/HeadString/HeadString.js
+++ src/components/GuitarHead/HeadStrings/HeadString/HeadString.js
@@ -2,10 +2,15 @@ import "./HeadString.scss";
 
 const HeadString = ({ id }) => {
   const headStringClass = `headString headString__${id}`;
+
+  const hoverString = () => {
+    console.log(`Hovering string ${id}`);
+  };
   return (
     <img
       className={headStringClass}
       src={require(`../../../../assets/images/guitar/GuitarNeck_String${id}.svg`)}
+      onMouseEnter={hoverString}
     ></img>
   );
 };
