c4baad82fa34ecf209261aac6cf820de584903c3
Martin Halas <marhaldev@gmail.com>
2022-02-22 12:06:35 -0500

    Initializing state in guitarcontainer

--- src/components/GuitarContainer/GuitarContainer.js
+++ src/components/GuitarContainer/GuitarContainer.js
@@ -1,8 +1,25 @@
 import "./GuitarContainer.scss";
 import Guitar from "./Guitar/Guitar.js";
 import UserInterface from "./UserInterface/UserInterface.js";
+import eStandardTuning from "../../assets/schema/constants";
+import React from "react";
 
 const GuitarContainer = () => {
+  const initialStrings = { ...eStandardTuning };
+
+  React.useEffect(() => {}, []);
+
+  const freqReducer = (state, action) => {
+    switch (action.type) {
+      case "FREQ_UP":
+        return state + 1;
+      case "FREQ_DOWN":
+        return state - 1;
+    }
+  };
+
+  const [freq, freqDispatch] = React.useReducer(freqReducer, 1);
+
   return (
     <div className="guitarContainer">
       <Guitar />
