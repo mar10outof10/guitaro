3cd3c37228b1b71925e39c0fc4df7d046ef3b0c3
Martin Halas <marhaldev@gmail.com>
2022-03-01 01:34:21 -0500

    Sound playing, layout working, needs a ton of refinement

--- src/components/GuitarContainer/UserInterface/StringsPanel/StringsPanel.js
+++ src/components/GuitarContainer/UserInterface/StringsPanel/StringsPanel.js
@@ -5,10 +5,12 @@ import { useStrings } from "../../../../hooks/stringsContext";
 const StringsPanel = () => {
   const { strings } = useStrings();
 
-  const stringItems = strings.map((string) => {
-    const { id, frequency } = { ...string };
-    return <StringsPanelItem key={id} id={id} frequency={frequency} />;
-  });
+  const stringItems = strings
+    .map((string) => {
+      const { id, frequency } = { ...string };
+      return <StringsPanelItem key={id} id={id} frequency={frequency} />;
+    })
+    .reverse();
 
   return <ol className="stringsPanel">{stringItems}</ol>;
 };
