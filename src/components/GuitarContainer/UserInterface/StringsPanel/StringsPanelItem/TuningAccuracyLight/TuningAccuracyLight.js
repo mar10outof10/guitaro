fb4e7e98528f0249029846835f58c3e4806c5e10
Martin Halas <marhaldev@gmail.com>
2022-03-04 14:26:42 -0500

    finished tuning accuracy light, centered ui string items

--- src/components/GuitarContainer/UserInterface/StringsPanel/StringsPanelItem/TuningAccuracyLight/TuningAccuracyLight.js
+++ src/components/GuitarContainer/UserInterface/StringsPanel/StringsPanelItem/TuningAccuracyLight/TuningAccuracyLight.js
@@ -9,24 +9,51 @@ const TuningAccuracyLight = ({ id, note, frequency }) => {
   const [lowRange, setLowRange] = React.useState(freqTable[note].low);
   const [highRange, setHighRange] = React.useState(freqTable[note].high);
   const [mid, setMid] = React.useState(freqTable[note].freq);
-  const [accuracy, setAccuracy] = React.useState();
+  const [accuracy, setAccuracy] = React.useState(100);
+  const [style, setStyle] = React.useState();
 
   React.useEffect(async () => {
     setMid(freqTable[note].freq);
-    setLowRange(mid - freqTable[note].low);
-    setHighRange(freqTable[note].high - mid);
+    setLowRange(freqTable[note].freq - freqTable[note].low);
+    setHighRange(freqTable[note].high - freqTable[note].freq);
   }, [note]);
 
   React.useEffect(() => {
     if (frequency >= mid) {
-      setAccuracy(((frequency - mid) / highRange).toFixed(2));
+      setAccuracy(100 - (((frequency - mid) / highRange) * 100).toFixed(0));
     } else {
-      setAccuracy(((mid - frequency) / lowRange).toFixed(2));
+      setAccuracy(
+        100 - Math.abs(((mid - frequency) / lowRange) * -100).toFixed(0)
+      );
     }
   }, [frequency]);
 
-  console.log(freqTable[note].freq, mid);
-  return <div className="TuningAccuracyLight">{accuracy}</div>;
+  React.useEffect(() => {
+    if (frequency > mid) {
+      setStyle({
+        background: `radial-gradient(circle at top, green, ${accuracy}%, transparent), radial-gradient(ellipse at bottom, red, #006f1600)`,
+      });
+    } else if (frequency < mid) {
+      setStyle({
+        background: `radial-gradient(circle at bottom, green, ${accuracy}%, transparent), radial-gradient(ellipse at top, red, #006f1600)`,
+      });
+    } else {
+      setStyle({
+        background: `radial-gradient(circle at center, green, ${accuracy}%, transparent), radial-gradient(ellipse at bottom, red, #006f1600)`,
+        "border-color": `#016200`,
+      });
+    }
+  }, [accuracy]);
+
+  console.log(
+    freqTable[note].low,
+    lowRange,
+    mid,
+    freqTable[note].freq,
+    freqTable[note].high,
+    highRange
+  );
+  return <div className="tuningAccuracyLight" style={style}></div>;
 };
 
 TuningAccuracyLight.propTypes = {
