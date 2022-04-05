ed668b8738b245671f4597f94b835c338136659a
Martin Halas <marhaldev@gmail.com>
2022-04-05 05:27:30 -0400

    Added touch drag functionality for mobile, need debouncing and mute

--- src/components/GuitarNeck/GuitarNeck.js
+++ src/components/GuitarNeck/GuitarNeck.js
@@ -2,9 +2,59 @@ import "./GuitarNeck.scss";
 import React from "react";
 import NeckStrings from "./NeckStrings/NeckStrings.js";
 
+import { useStrings } from "../../hooks/stringsContext";
+import { useAudio } from "../../hooks/audioContext";
+
+import { playFrequency } from "../../utils/audioFunctions";
+
 const GuitarNeck = React.memo(function GuitarNeck() {
+  const neckRef = React.useRef(null);
+
+  const { strings } = useStrings();
+  const { audio } = useAudio();
+
+  const stringXCoordinates = [113, 127, 139, 153, 166, 176];
+
+  const [touchPos, setTouchPos] = React.useState(null);
+
+  const handleTouchStart = (e) => {
+    if (audio.mute) {
+      e.preventDefault();
+      return;
+    }
+    setTouchPos(e.changedTouches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    const currX = e.changedTouches[0].clientX;
+    const prevIndex = stringXCoordinates.findIndex((coord) => coord > touchPos);
+    const currIndex = stringXCoordinates.findIndex((coord) => coord > currX);
+    console.log(touchPos, currX);
+    if (prevIndex !== currIndex) {
+      let lowIndex = Math.min(prevIndex, currIndex);
+      let highIndex = Math.max(prevIndex, currIndex);
+      if (lowIndex === -1) {
+        [lowIndex, highIndex] = [highIndex, 6];
+      }
+      for (let i = lowIndex; i < highIndex; i++) {
+        playFrequency(strings[i].frequency);
+      }
+    }
+    setTouchPos(currX);
+  };
+
+  const handleTouchEnd = () => {
+    setTouchPos(null);
+  };
+
   return (
-    <div className="guitarNeck">
+    <div
+      className="guitarNeck"
+      ref={neckRef}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <NeckStrings />
     </div>
   );
