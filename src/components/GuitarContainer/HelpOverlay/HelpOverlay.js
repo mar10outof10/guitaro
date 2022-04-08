e09c973b7566abf2911b43398d31079423ead204
Martin Halas <marhaldev@gmail.com>
2022-04-08 03:36:58 -0400

    added window resize hook to maintain ui overlay on window resize

--- src/components/GuitarContainer/HelpOverlay/HelpOverlay.js
+++ src/components/GuitarContainer/HelpOverlay/HelpOverlay.js
@@ -1,23 +1,43 @@
 import "./HelpOverlay.scss";
 import React from "react";
 import { useOverlay } from "../../../hooks/overlayContext";
+import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
 
 const HelpOverlay = function HelpOverlay() {
   const { overlay, overlayDispatch } = useOverlay();
+
+  const { width } = useWindowDimensions();
 
   const overlayClassName = `helpOverlay helpOverlay__${
     overlay.active ? "active" : "inactive"
   }`;
 
-  const OverlayImage = () => (
-    <img
-      src={require(`../../../assets/images/help_overlay${
+  const [overlaySrc, setOverlaySrc] = React.useState(
+    require(`../../../assets/images/help_overlay${
+      window.innerWidth <= 600 ? "_mobile_closed" : ""
+    }.png`)
+  );
+
+  const windowWidth = window.innerWidth;
+
+  React.useEffect(() => {
+    console.log(width);
+    setOverlaySrc(
+      require(`../../../assets/images/help_overlay${
         window.innerWidth <= 600
           ? overlay.stringsPanelOpen
             ? "_mobile_open"
             : "_mobile_closed"
           : ""
-      }.png`)}
+      }.png`)
+    );
+  }, [width, overlay.stringsPanelOpen]);
+
+  console.log(overlaySrc);
+
+  const OverlayImage = () => (
+    <img
+      src={overlaySrc}
       alt="How-to overlay"
       draggable="false"
       onClick={() => overlayDispatch({ type: "TOGGLE_OVERLAY" })}
