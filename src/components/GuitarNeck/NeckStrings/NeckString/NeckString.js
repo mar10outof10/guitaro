1e62ee63206597b62d53d934807ae0e3f4d30b66
Martin Halas <marhaldev@gmail.com>
2022-04-08 01:54:02 -0400

    Removed deprecated play function and imports from NeckString.js

--- src/components/GuitarNeck/NeckStrings/NeckString/NeckString.js
+++ src/components/GuitarNeck/NeckStrings/NeckString/NeckString.js
@@ -1,25 +1,14 @@
 import "./NeckString.scss";
 import React from "react";
 import PropTypes from "prop-types";
-import { useStrings } from "../../../../hooks/stringsContext";
-import { useAudio } from "../../../../hooks/audioContext";
-import { playFrequency } from "../../../../utils/audioFunctions";
 
 const NeckString = React.memo(function NeckString({ id }) {
-  const { strings } = useStrings();
-  const { audio } = useAudio();
-
   const neckStringClass = `neckString neckString__${id}`;
-
-  const stringFreq = strings[id - 1].frequency;
 
   return (
     <img
       className={neckStringClass}
       src={require(`../../../../assets/images/guitar/GuitarNeck_String${id}.svg`)}
-      onMouseEnter={() => {
-        audio.mute ? null : playFrequency(stringFreq, audio.volume);
-      }}
     />
   );
 });
