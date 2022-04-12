58164501a930188ab7855382b03a0ae6a4dc7347
Martin Halas <marhaldev@gmail.com>
2022-04-11 20:09:41 -0400

    Added github logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,22 @@ import HeaderOptions from "./HeaderOptions/HeaderOptions.js";
 const Header = React.memo(function Header() {
   return (
     <header className="header">
-      <div className="header_logoContainer">
+      <div className="header__logoContainer">
         <img
-          className="header_logo"
+          className="header__logo"
           src={require("../../assets/images/guitaro_logo_header.png")}
         ></img>
+        <a
+          className="header__githubLink"
+          href="https://github.com/mar10outof10"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="header__githubLogo"
+            src={require("../../assets/images/github_logo.png")}
+          ></img>
+        </a>
       </div>
       <HeaderOptions />
     </header>
